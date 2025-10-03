d61472a45090098b521565aa5d002ab988a4f669
Ash <ohhaashis@gmail.com>
2025-10-03 09:16:51 +0000

    Add5

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1) => true
+// console.log("2">1)  => true
+
+// console.log(null >0)
+// console.log(null ==0)
+// console.log(null >=0)
+// console.log(null <0)
+// console.log(null <=0)
+/*false
+false
+true
+false
+true*/
+
+// === 
+// console.log("2"==2); // => true
+// console.log("2"===2);// => false
+
