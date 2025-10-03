dece83c6b7b7a30a9e613d16a46e0e00bdb2dc4a
Ash <ohhaashis@gmail.com>
2025-10-03 08:19:33 +0000

    Add2

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+console.log("Hi")
+let name = "Ash"
+let age = 18
+let isLoggerIn = false
+
+//number => 2 to power53
+// bigint 
+// string => ""
+// boolean => true/false
+// null  => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null)//object
+// null => object
