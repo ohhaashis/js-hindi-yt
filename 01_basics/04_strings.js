be982626c651a36a4e0085dfd6a7990a9def183e
Ash <ohhaashis@gmail.com>
2025-10-04 06:01:22 +0000

    Add9

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,31 @@
+// const name = "Ash"
+// const repoCount = 18
+// // console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+
+// const gameName = new String('Ash-2909')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName);
+// console.log(gameName.charAt('1'));
+// console.log(gameName.indexOf('s'));
+
+// const newStr = gameName.substring(0,3);
+// console.log(newStr);
+
+// const anotherStr = gameName.slice(0,3);
+// console.log(anotherStr);
+
+// const anotherStr1 = gameName.slice(-8,3);
+// console.log(anotherStr1);
+
+
+// const NewString = gameName.slice(-8,4);
+// console.log(NewString);
+
+
+const newStringOne = '  Ash  2909  '
