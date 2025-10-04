054c19315b6a8b43dc43a4dd8f05cdc9fa16f3b8
Ash <ohhaashis@gmail.com>
2025-10-04 18:01:26 +0000

    Add11

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+
+// const myArr = [0,1,2,3,4,5,true,"Ash"]
+// const myHeros = ["Ash","Sum"]
+
+// console.log(myArr[0]);
+
+// const arr1 = new  Array(1,2,3,4)
+// console.log(arr1[1]); // => 2
+
+// Array methods
+// arr1.push(6)
+// console.log(arr1);
+// arr1.pop()
+// console.log(arr1);
+// arr1.unshift(0);
+// console.log(arr1)
+
+// arr1.shift(0)
+// console.log(arr1);
+
+//console.log(arr1.includes(9)) => false
+
+// const newArr = arr1.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+
+// slice , spice
+
+const arr1 = new  Array(1,2,3,4)
+
+console.log("A ",arr1);
+
+const myn1 = arr1.slice(1,3)
+
+console.log(myn1);
+
+
+const myn2 = arr1.splice(1,3) // 
+console.log(arr1)
+console.log(myn2);
