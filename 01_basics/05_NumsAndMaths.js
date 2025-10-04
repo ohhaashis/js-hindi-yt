97765b83ee9b0835c7a8e68f924f5ce5acbf2198
Ash <ohhaashis@gmail.com>
2025-10-04 06:27:38 +0000

    Add11

---
+++ 01_basics/05_NumsAndMaths.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score); // 400
+// const balance = new Number(100)
+// console.log(balance);// Number : 400
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// const otherNum = 123.8966
+
+// console.log(otherNum.toPrecision(4))
+;
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.2));
+// console.log(Math.ceil(4.3));
+
+
+// console.log((Math.random()*10) +1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.random()*(max-min+1)+ min);
