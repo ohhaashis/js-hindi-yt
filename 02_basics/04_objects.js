e9ca6b12e07f08d271da36e74acb655f32f48231
Ash <ohhaashis@gmail.com>
2025-10-07 16:35:16 +0000

    Add15

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,50 @@
+// const tinderUser =  new Object()
+// tinderUser.id = "123abc"
+// tinderUser.name = "Ash"
+// tinderUser.isLoggedin = false
+
+// // console.log(tinderUser);
+
+// // //console.log(tinderUser)
+
+// // const regularUser = {
+// //     email : "Ash@gmail.com",
+// //     fullname:{
+// //         userfullname:{
+// //             firstname : "Ash",
+
+// //         }
+// //     }
+// // };
+// // console.log(regularUser.fullname);
+
+// // const obj1 = {1:"a",2:"b"};
+// // const obj2 = {3:"c",4:"d"};
+
+// // const obj3 = { obj1,obj2}
+// // console.log(obj3);
+
+// // const obj3 = Object.assign({},obj1,obj2);
+// // console.log(obj3);
+
+
+
+// const users = [
+//     {
+//         id : 1,
+//         email : "Ash@gmail.com",
+//     },
+//     {
+        
+//     },
+// ]
+
+
+// users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedin'));
