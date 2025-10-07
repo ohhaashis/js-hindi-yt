7c939fa097e18a77bda9a9a7d1e78b27344cfd9e
Ash <ohhaashis@gmail.com>
2025-10-07 17:20:14 +0000

    Add17

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,43 @@
+// function addTwoNum(n1,n2){//-> parameters
+//      console.log(n1+n2);
+// }
+// addTwoNum(3,4)// -> arguments
+
+// function loginUserMsg(username){
+//     return `${username} just logged In !`
+// };
+
+// console.log(loginUserMsg("Ash"));
+
+// function calcCartPrice(num1){
+//  return num1;
+// }
+// console.log(calcCartPrice(200,300,400));// --> 200
+
+
+// -> convertin into array by dots ...
+// function calcCartPrice(...num1){ // tripple dots
+//    return num1;
+// };
+// console.log(calcCartPrice(200,300,400)); // -> [200,300,400]
+
+// const user ={
+//     username : "Ash",
+//     price : 199,
+// };
+
+// function handleObject(anyobject){
+//   console.log(`Username is ${anyobject.username} and price is ${anyobject.price}.`)
+// }
+
+// handleObject(user);
+
+
+// const myNewArr =[200,300,400,500]
+
+// function returnSecondvalue(getArray){
+//     return getArray[1];
+// }
+// console.log(returnSecondvalue(myNewArr)); // same
+// console.log(returnSecondvalue([200,300,400,500]));// same
+
