dd24a51f47936aeb1cf9204a63706c795dc302e2
Ash <ohhaashis@gmail.com>
2025-10-07 17:38:44 +0000

    Add18

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,46 @@
+// let a = 20;
+// const b = 20;
+// var c = 30;
+
+// if(true){
+//    let a = 20;
+//    const b = 20;
+//    var c = 30;
+// }
+
+// console.log(a) // not defined
+// console.log(b)// not defined
+// console.log(c)// => defined
+
+
+// global scope , local scope 
+
+// function one(){
+//     const username = "Ash";
+
+//     function two(){
+//         const website = "Ash";
+//         console.log(username);
+//     }
+//     // console.log(website);
+
+//     two()
+// }
+
+// one()
+
+
+//+++++++++++++++Interesting+++++++++++++++++
+
+// function addOne(num){
+//     return num+1;
+// }
+
+// addOne(5);
+
+// const adTwo = function(num){
+//     return num+2;
+// }
+// console.log(adTwo(5));
+
+
