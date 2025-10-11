99ffcd947f07be0374378d4743dce9e564f0cd88
Ash <ohhaashis@gmail.com>
2025-10-11 17:35:12 +0000

    Add20

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// IIFE => Immediately Invoked Function Expressions 
+// jo function immediately execute ho jaye 
+// global scope ke pollution se problem hoti kai bar
+// to use declaration or global scopes ke polltuion
+//  ko hatane ke liye we use IIFE
+
+
+// (function chai(){
+//     console.log(` DB connected`);
+// })()
+
+// chai()
+
+// (()=>{
+//     console.log(`Db Connecetd`);
+// })
