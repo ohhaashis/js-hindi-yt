593c29be3b1846fb3342a24e351d469ff631af1e
Ash <ohhaashis@gmail.com>
2025-10-30 15:34:59 +0000

    Add25

--- 05_Iterations/five.js
+++ 05_Iterations/five.js
@@ -1,5 +1,11 @@
-const coding = [ "js","cpp","py","xb","swift","java"]
+// const coding = [ "js","cpp","py","xb","swift","java"]
 
-coding.forEach(  function (item){
-    console.log(item);
-})
+// coding.forEach(  function (item){
+//     console.log(item);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// }
+// )
+
