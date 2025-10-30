f8f0639f6d9f5c31f1f79c3cd7398b58920cfa82
Ash <ohhaashis@gmail.com>
2025-10-30 15:04:19 +0000

    Add23

---
+++ 05_Iterations/one.js
@@ -0,0 +1,28 @@
+// for
+
+// for(let index = 0; index <= 10 ; index++){
+
+//     const element = index;
+//     console.log(element); 
+// }
+
+// console.log("ASH");
+
+// for(let i = 0 ; i<=10 ; i++){
+//     console.log(`Outer loop value : ${i}`);
+//     for(let j=0; j <=10 ; j++){
+//         console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     }
+// }
+
+//  Keywords
+
+// break and continue
+
+for(let i=0 ; i<=10 ; i++){
+     console.log(`Value : ${i}`);
+    if(i==5){
+        console.log(`Detected : ${i}`)
+        break;
+    }
+}
